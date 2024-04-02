190570e8abcefaae51b9ec0306082134e2ae5312
Rebecca <remazzoc@lakeheadu.ca>
2024-04-02 19:35:51 -0400

    week 13 app api

---
+++ assignment04/scripts/script.js
@@ -0,0 +1,97 @@
+const url = 'https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '00e595db80mshd049170895809cep161b91jsn55593ddd528a',
+		'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
+	}
+};
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+// // The URL for the Article Search API at nytimes.com
+// const baseURL = 'https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat';
+// // STEP 1: Get your own API key and paste it below…
+// const key = 'ajHuiZdBc3J89usJJyWocrCGnSFPDfPW';
+// let url;
+// // Grab references to all the DOM elements you'll need to manipulate
+// const searchTerm = document.querySelector('.search');
+// const startDate = document.querySelector('.start-date');
+// const endDate = document.querySelector('.end-date');
+// const searchForm = document.querySelector('form');
+// const submitBtn = document.querySelector('.submit');
+// const section = document.querySelector('section');
+// // STEP 2: Add a submit event listener for the search form, 
+// //referencing the fetchResults function as the callback
+// submitBtn.addEventListener("click", fetchResults);
+
+// // Functions
+// function fetchResults(event) {
+//     // Use preventDefault() to stop the form submitting
+//     event.preventDefault();
+//     // STEP 3: Assemble the full URL, according to the API documentation at the New York Times
+//     url=`${baseURL}?q=${searchTerm.value}&api-key=${key}`;
+//     if(startDate.value !==""){
+//         url += '&begin_date=' + startDate.value;
+//     }
+//     if(endDate.value !==""){
+//         url+= '&end_date=' + endDate.value;
+
+//     }
+//     console.log(url);
+//     // STEP 4: Use fetch() to pass the URL that we built as a request to the API service, 
+//     //then pass the JSON to the displayResults() function
+//     fetch(url).then(response => {
+//         return response.json();
+//     }).then(json => displayResults(json));
+// };
+
+// function displayResults(json) {
+//     // STEP 5: Log to the console the results from the API
+//     console.log(json);
+
+//     // Clear out the old results…
+//     while (section.firstChild) {
+//         section.removeChild(section.firstChild);
+//     };
+//     // STEP 6: Create the variable articles to capture the articles from the JSON object
+//     let articles=json.response.docs;
+
+//     if (articles.length === 0) {
+//         const para = document.createElement('p');
+//         para.textContent = 'No results returned.'
+//         section.appendChild(para);
+//     } else {
+//         for (let i = 0; i < articles.length; i++) {
+//             const article = document.createElement('article');
+//             const heading = document.createElement('h2');
+//             const link = document.createElement('a');
+//             const img = document.createElement('img');
+//             const para1 = document.createElement('p');
+
+//             const current = articles[i];
+//             console.log(current);
+//             // STEP 7: Look at the console output from the API…
+//             link.href = current.web_url;
+//             link.textContent = current.headline.main;
+//             para1.textContent = current.snippet;
+
+//             if (current.multimedia.length > 0) {
+//                 img.src = 'https://www.nytimes.com/' + current.multimedia[0].url;
+//                 img.alt = current.headline.main;
+//             };
+//             // STEP 8: Put each article together as an ARTICLE element and append it as a child of the SECTION element in the HTML
+//             article.appendChild(heading);
+//             heading.appendChild(link);
+//             article.appendChild(img);
+//             article.appendChild(para1);
+//             section.appendChild(article);
+//         };
+//     };
+// };
+
+// // This example adapted from "Third-party APIs" at https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Third_party_APIs
